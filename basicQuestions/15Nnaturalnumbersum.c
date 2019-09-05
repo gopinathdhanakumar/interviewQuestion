8d1d348165f3042ede74e43fc988c82394fcdf84
gopinathdhanakumar <54261094+gopinathdhanakumar@users.noreply.github.com>
2019-09-06 00:57:23 +0530

    Create 15Nnaturalnumbersum.c

---
+++ basicQuestions/15Nnaturalnumbersum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   printf("enter the number of natural number....");
+   scanf("%d",&a);
+   printf("sum of %d natural number is  = %d",a,(a*(a+1))/2);
+}
