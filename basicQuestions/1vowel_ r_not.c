6e191ab0ecb79bc942b07c3c16adafd4449aec63
gopinathdhanakumar <54261094+gopinathdhanakumar@users.noreply.github.com>
2019-09-05 23:49:45 +0530

    Update and rename vowel_ r_not.c to 1vowel_ r_not.c

--- basicQuestions/1vowel_ r_not.c
+++ basicQuestions/1vowel_ r_not.c
@@ -4,7 +4,7 @@ int main()
 {
    
    char a;
-   printf("Enter the character")
+   printf("Enter the character..=");
    scanf("%c",&a);
    if(a=='a' || a=='e' || a=='i' || a=='o' ||a=='u' || 
        a=='A' || a=='E' || a=='I' || a=='O' || a=='U')
