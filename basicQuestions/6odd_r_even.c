9c24daf89124253802ee4a9a157131d09a389b13
gopinathdhanakumar <54261094+gopinathdhanakumar@users.noreply.github.com>
2019-09-06 00:26:45 +0530

    Create 6odd_r_even.c

---
+++ basicQuestions/6odd_r_even.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   printf("enter the number....");
+   scanf("%d",&a);
+   if(a&1)
+   {
+       printf("\n%d is odd number",a);
+   }
+   else
+   {
+   
+   printf("\n%d is a even number",a);
+   }
+}
