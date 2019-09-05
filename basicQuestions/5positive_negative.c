748ed181c88d364cf74b29e97cac2560e7e02a51
gopinathdhanakumar <54261094+gopinathdhanakumar@users.noreply.github.com>
2019-09-06 00:25:05 +0530

    Create 5positive_negative.c

---
+++ basicQuestions/5positive_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   printf("enter the number....");
+   scanf("%d",&a);
+   if(a<0)
+   {
+       printf("\n%d is negative number",a);
+   }
+   else
+   {
+   
+   printf("\n%d is a positive number",a);
+   }
+}
